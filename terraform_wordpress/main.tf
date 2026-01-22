3f1b8ed09d0a9882dbeb6b61a179416d4917c78f
Mac <mac@Macs-MacBook-Pro.local>
2026-01-22 14:19:20 +0100

    updated the regions for fix

--- terraform_wordpress/main.tf
+++ terraform_wordpress/main.tf
@@ -15,6 +15,7 @@ terraform {
 module "vpc" {
   source   = "./modules/vpc"
   vpc_cidr = "10.0.0.0/16"
+  region = var.region
 }
 
 module "security" {
--- terraform_wordpress/modules/vpc/main.tf
+++ terraform_wordpress/modules/vpc/main.tf
@@ -19,14 +19,14 @@ resource "aws_subnet" "public" {
 resource "aws_subnet" "private_1" {
   vpc_id     = aws_vpc.this.id
   cidr_block = "10.0.2.0/24"
-  availability_zone = "us-east-1a"
+  availability_zone = "${var.region}a"
 
 }
 
 resource "aws_subnet" "private_2" {
   vpc_id     = aws_vpc.this.id
   cidr_block = "10.0.3.0/24"
-  availability_zone = "us-east-1b"
+  availability_zone = "${var.region}b"
 
 }
 
