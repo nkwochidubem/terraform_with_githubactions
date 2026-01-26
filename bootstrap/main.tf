f219d649cb786008741b46625445f4ef7acefbce
Mac <mac@Macs-MacBook-Pro.local>
2026-01-26 12:14:30 +0100

    new oidc setup

--- bootstrap/main.tf
+++ bootstrap/main.tf
@@ -33,18 +33,6 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "state_encryption"
   }
 }
 
-# 4. DynamoDB Table for State Locking
-resource "aws_dynamodb_table" "terraform_locks" {
-  name         = "terraform-locks"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
-
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-}
-
 
 
 
